b677d47e9731dfab6de8234a02f802a75b8cb186
ramsey6340 <eniac5project@gmail.com>
2023-09-06 11:24:38 +0000

    configuration de firebase dans l'app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDW9MK1pAcfgLo_M8w93nrcEXKrZ3FUclM",
+    authDomain: "masjid-1f3cf.firebaseapp.com",
+    projectId: "masjid-1f3cf",
+    storageBucket: "masjid-1f3cf.appspot.com",
+    messagingSenderId: "624648395471",
+    appId: "1:624648395471:web:bd6b20f1d66ae6038d35ba",
+    measurementId: "G-LMJLEJSFCJ"
+  }
 };
 
 /*
