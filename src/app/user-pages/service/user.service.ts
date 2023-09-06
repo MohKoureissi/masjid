771d2e52c75e5ee6cf7d1f5bb7d3008dec21d438
ayaya <ayaya6436@gmail.com>
2023-09-06 16:35:37 +0000

    begin all methods users

--- src/app/user-pages/service/user.service.ts
+++ src/app/user-pages/service/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,20 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   constructor() { }
+
+  register(email:string, password:string) {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+  }
+
 }
