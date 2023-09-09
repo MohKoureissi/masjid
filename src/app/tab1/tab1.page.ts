fb9f25f057d7e6ac7520b5ddf0bc5a20a147abd1
ayaya <ayaya6436@gmail.com>
2023-09-09 11:58:58 +0000

    display all mosquee

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,12 +11,14 @@ import 'leaflet-routing-machine';
 })
 export class Tab1Page implements OnInit {
   map!: L.Map;
+  currentRoute: any;
+
   constructor(private mosqueService: MosqueService) {}
 
   async ngOnInit() {
- this.initMap()
- this.displayMosques()
-
+    await this.initMap();
+    const mosques = await this.mosqueService.filter();
+    await this.displayAllMosques(mosques);
   }
 
   async initMap() {
@@ -35,33 +37,47 @@ export class Tab1Page implements OnInit {
     marker.bindPopup('Vous êtes ici !').openPopup();
   }
 
-  async displayMosques() {
-    // Récupérez les données des mosquées depuis le service
-    const mosques = await this.mosqueService.filter();
-
-    // Obtenir la position actuelle
-    const { coords } = await Geolocation.getCurrentPosition();
-    const lat = coords.latitude;
-    const lng = coords.longitude;
-
-    // Créer un itinéraire depuis votre position actuelle jusqu'à chaque mosquée
+  async displayAllMosques(mosques: any[]) {
     mosques.forEach((mosque) => {
       const mosqueLocation = L.latLng(mosque.lat, mosque.lng);
-      const route = L.Routing.control({
-        waypoints: [
-          L.latLng(lat, lng), // Position actuelle
-          mosqueLocation,     // Emplacement de la mosquée
-        ],
-        routeWhileDragging: true, // Mettez à true si vous souhaitez recalculer l'itinéraire pendant le glissement du marqueur
-      }).addTo(this.map);
 
       // Ajoutez un marqueur à la position de la mosquée
-      L.marker(mosqueLocation)
+      const mosqueMarker = L.marker(mosqueLocation)
         .addTo(this.map)
-        .bindPopup(`Nom : ${mosque.name}<br>Quartier : ${mosque.quartier}`);
+        .bindPopup(`Nom : ${mosque.name}<br>Quartier : ${mosque.quartier} <br>Quartier : ${mosque.imanName}`);
+
+      // Attachez un gestionnaire d'événements de clic au marqueur pour lancer l'itinéraire
+      mosqueMarker.on('click', () => {
+        this.launchRouteToMosque(mosque);
+      });
     });
   }
 
-  
+  async createRoute(startCoords: L.LatLng, endCoords: L.LatLng) {
+    // Supprimez l'itinéraire actuel s'il existe
+    if (this.currentRoute) {
+      this.map.removeControl(this.currentRoute);
+    }
 
+    // Calculez un nouvel itinéraire
+    this.currentRoute = L.Routing.control({
+      waypoints: [
+        L.latLng(startCoords.lat, startCoords.lng), // Point de départ
+        L.latLng(endCoords.lat, endCoords.lng),     // Point d'arrivée
+      ],
+      routeWhileDragging: true, // Mettez à true si vous souhaitez recalculer l'itinéraire pendant le glissement du marqueur
+    }).addTo(this.map);
+  }
+
+  async launchRouteToMosque(mosque: any) {
+    // Obtenez les coordonnées de la mosquée
+    const mosqueLocation = L.latLng(mosque.lat, mosque.lng);
+
+    // Obtenir la position actuelle de l'utilisateur
+    const { coords } = await Geolocation.getCurrentPosition();
+    const currentLocation = L.latLng(coords.latitude, coords.longitude);
+
+    // Créez un itinéraire depuis la position actuelle vers la mosquée
+    this.createRoute(currentLocation, mosqueLocation);
+  }
 }
