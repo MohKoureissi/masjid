c2f353c1c78b64f372a54fb4451c8fe330abe630
SouleymaneFOMBA <babalarab2012@gmail.com>
2023-09-15 09:06:36 +0000

    Date important

--- src/app/calendar/calendar.page.ts
+++ src/app/calendar/calendar.page.ts
@@ -16,7 +16,7 @@ onSelect(event: Date){
   this.selectedDate= event;
 }
 selectedDate: Date = new Date(); // Date sélectionnée par défaut
-selectedDate1: Date = new Date(1445,2,14);
+selectedDate1: Date = new Date(14,2,14);
 // selectedDate: Date = new Date();
 // selectedDate: Date = new Date();
 
